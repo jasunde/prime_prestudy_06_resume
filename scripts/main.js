9615f2514a3e5f3832840707b169b1ab725774b3
jasunde <jlaurits@gmail.com>
2016-10-23 09:19:45 -0500

    Changed behavior to hide first

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,9 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-
-$(document).ready(function () {
+// $(document).ready(function () {
   var item = $('header h1').first(),
       title = item.text(),
       newTitle = '',
       i = 0;
 
-  addToString(0, newTitle);
 
   function addToString(ind, newTitle) {
     scramble(title[ind], newTitle);
@@ -82,7 +78,7 @@ $(document).ready(function () {
   hide.on('click', function (e) {
     $(this).text(main.css('display') === 'none' ? 'Hide the Resume' : 'Show the Resume');
     main.toggle();
+  }).one('click', function (e) {
+    addToString(0, newTitle);
   });
-});
-
-},{}]},{},[1]);
+// });
